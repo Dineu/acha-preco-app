6db54f9d19e1b84a6ca170bd758f8f0d582b6838
Dineu Balduino Oliveira <dineu.balduino.oliveira@gmail.com>
2025-08-13 14:26:06 +0000

    quando a tela está expandida o menu desaparece

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -260,7 +260,7 @@ const Sidebar = React.forwardRef<
     return (
       <div
         ref={ref}
-        className="group peer md:block text-sidebar-foreground"
+        className="group peer hidden md:block text-sidebar-foreground"
         data-state={state}
         data-collapsible={state === "collapsed" ? collapsible : ""}
         data-variant={variant}
@@ -812,3 +812,5 @@ export {
   SidebarTrigger,
   useSidebar,
 }
+
+    
