e886fdd985be316c675b31856fdc1b5873fce316
Dineu Balduino Oliveira <dineu.balduino.oliveira@gmail.com>
2025-08-13 11:14:27 +0000

    isso não parece etiqueta

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,9 +12,17 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48" className="text-primary" />
-      <circle cx="10.5" cy="10.5" r="7.5" className="stroke-primary-foreground" />
-      <line x1="21" y1="21" x2="15.8" y2="15.8" className="stroke-primary-foreground" />
+      {/* Lupa */}
+      <circle cx="10.5" cy="10.5" r="7.5" className="text-primary" />
+      <line x1="21" y1="21" x2="15.8" y2="15.8" className="text-primary" />
+      
+      {/* Carrinho de Compras dentro da Lupa */}
+      <g className="stroke-primary-foreground">
+        <circle cx="8" cy="14" r="1" />
+        <circle cx="13" cy="14" r="1" />
+        <path d="M5.5 12h10l-1.5-6H7L5.5 12z" />
+        <path d="M5.5 12H4.5a1 1 0 0 0-1 1v1h12v-2" />
+      </g>
     </svg>
   );
 }
