c2926f40296c71a6724280dd25af5d2d02805f1a
Dineu Balduino Oliveira <dineu.balduino.oliveira@gmail.com>
2025-08-13 13:42:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,16 +11,17 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      {/* Lupa (baseada no desenho) */}
-      <g className="text-primary" strokeWidth="2.5">
+      {/* Lupa */}
+      <g stroke="hsl(var(--primary))" strokeWidth="2.5">
         <circle cx="10.5" cy="10.5" r="8.5" />
         <line x1="16.5" y1="16.5" x2="22" y2="22" />
       </g>
       
-      {/* Carrinho de Compras dentro da Lupa */}
+      {/* Carrinho de Compras */}
       <g 
         transform="translate(5.5, 6.5) scale(0.4)" 
-        className="text-primary-foreground"
+        stroke="hsl(var(--primary-foreground))"
+        fill="none"
         strokeWidth="2.5"
       >
         <circle cx="6" cy="21" r="1" />
