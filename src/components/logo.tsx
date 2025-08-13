bee6ad54383aea22747f6f06f4d622b9e3067012
Dineu Balduino Oliveira <dineu.balduino.oliveira@gmail.com>
2025-08-13 11:19:48 +0000

    aumente o tamanho da lupa para que fique um mínimo de espaço ao redor do

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -17,7 +17,7 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       <line x1="17" y1="17" x2="21" y2="21" className="text-primary" />
       
       {/* Carrinho de Compras (dentro da lupa) - Menor */}
-      <g className="stroke-primary-foreground" transform="translate(5, 5.5) scale(0.5)" strokeWidth="2.5">
+      <g className="stroke-primary-foreground" transform="translate(4.5, 4) scale(0.6)" strokeWidth="2.5">
         <circle cx="9" cy="21" r="1" />
         <circle cx="20" cy="21" r="1" />
         <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
