0c3d8a598e812ee54b6ab83fd88b89fd377bf24f
Dineu Balduino Oliveira <dineu.balduino.oliveira@gmail.com>
2025-08-13 11:12:31 +0000

    colocar um ícone melhor para o APP

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,9 +12,9 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M21 12a9 9 0 1 1-6.219-8.56" className="text-primary" />
-      <path d="m2.5 10.5 5 5" className="stroke-primary-foreground" />
-      <path d="M12 2a4 4 0 0 0-4 4v0a4 4 0 0 0 4 4Z" className="stroke-primary-foreground" />
+      <path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48" className="text-primary" />
+      <circle cx="10.5" cy="10.5" r="7.5" className="stroke-primary-foreground" />
+      <line x1="21" y1="21" x2="15.8" y2="15.8" className="stroke-primary-foreground" />
     </svg>
   );
 }
